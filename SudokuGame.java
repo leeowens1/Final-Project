4c47179448e5c2b8df48118bd0df26d02010ef24
leeowens1 <31625947+leeowens1@users.noreply.github.com>
2017-12-03 21:08:43 -0500

    Buttons

--- SudokuGame.java
+++ SudokuGame.java
@@ -2,19 +2,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
-import java.awt.event.KeyListener;
+
 
 public class SudokuGame{
 	SudokuDB SDB = new SudokuDB();						
 	SudokuGUI SGUID = new SudokuGUI();					
 	ButtonListener listener = new ButtonListener();	// Create BUTT Listener Object
-	KeyListener listener = new KeyListener
 	
-	// Customer vars
-	//int CustomerCount;
-	//int CustomerPosition; 
-	//int CustomerFormState = 0;  // 0 standard, 1 add, 2 edit
-	//ArrayList<Integer> CustomerIds = new ArrayList<Integer>();
 	
 	public static void main(String args[]) {
 		new SalesExpress();
@@ -24,10 +18,9 @@ public SudokuGame(){
 		SDB.connectDB();
 		SDB.createTables();
 		SGUID.ConstructGUI();
-		
-		
-		
-	SGUID.NewGameBut.addActionListener(listener);
+
+	
+	
 	
 		
-	}
+}
